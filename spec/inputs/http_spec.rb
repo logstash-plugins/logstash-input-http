2891a4eb84c7599d0e5d6051958a357efca3a1b3
Joao Duarte <jsvduarte@gmail.com>
2015-05-15 18:21:19 +0100

    allow applying codecs depending on content-type
    Fixes #4

--- spec/inputs/http_spec.rb
+++ spec/inputs/http_spec.rb
@@ -2,6 +2,7 @@ require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/http"
 require "json"
 require "ftw"
+require "stud/temporary"
 
 describe LogStash::Inputs::Http do
 
@@ -10,6 +11,32 @@ describe LogStash::Inputs::Http do
 
   after :each do
     subject.teardown
+  end
+
+  context "with default codec" do
+    subject { LogStash::Inputs::Http.new }
+    context "when receiving a text/plain request" do
+      it "should process the request normally" do
+        subject.register
+        Thread.new { subject.run(queue) }
+        agent.post!("http://localhost:8080/meh.json",
+                    :headers => { "content-type" => "text/plain" },
+                    :body => "hello")
+        event = queue.pop
+        expect(event["message"]).to eq("hello")
+      end
+    end
+    context "when receiving an application/json request" do
+      it "should parse the json body" do
+        subject.register
+        Thread.new { subject.run(queue) }
+        agent.post!("http://localhost:8080/meh.json",
+                    :headers => { "content-type" => "application/json" },
+                    :body => { "message_body" => "Hello" }.to_json)
+        event = queue.pop
+        expect(event["message_body"]).to eq("Hello")
+      end
+    end
   end
 
   context "with json codec" do
@@ -37,8 +64,9 @@ describe LogStash::Inputs::Http do
       end
     end
     context "with :keystore and :keystore_password" do
+      let(:keystore) { Stud::Temporary.file }
       subject { LogStash::Inputs::Http.new("ssl" => true,
-                                           "keystore" => "/tmp/keystore.jks",
+                                           "keystore" => keystore.path,
                                            "keystore_password" => "pass") }
       it "should not raise exception" do
         expect { subject.register }.to_not raise_exception
