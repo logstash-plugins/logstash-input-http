879c1f8731858f088779beafee9b54a2e74b93e8
kares <self@kares.org>
2022-03-16 12:36:21 +0100

    more comments on CHACHA20

--- src/main/java/org/logstash/plugins/inputs/http/util/SslSimpleBuilder.java
+++ src/main/java/org/logstash/plugins/inputs/http/util/SslSimpleBuilder.java
@@ -39,12 +39,12 @@ public class SslSimpleBuilder implements SslBuilder {
             // Modern compatibility
             "TLS_AES_128_GCM_SHA256", // TLS 1.3
             "TLS_AES_256_GCM_SHA384", // TLS 1.3
-            "TLS_CHACHA20_POLY1305_SHA256", // TLS 1.3 (since Java 11)
+            "TLS_CHACHA20_POLY1305_SHA256", // TLS 1.3 (since Java 11.0.14)
             // Intermediate compatibility
             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
             "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256", // (since Java 11)
-            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256", // (since Java 11)
+            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256", // (since Java 11.0.14)
+            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256", // (since Java 11.0.14)
             "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
             "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
             // Backward compatibility
