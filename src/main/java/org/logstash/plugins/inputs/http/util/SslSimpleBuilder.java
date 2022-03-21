d4b71f257acb18d7a7a22fc6f032767f05e7d005
kares <self@kares.org>
2022-03-21 14:15:16 +0100

    Refactor: re-order cipher preference + docs

--- src/main/java/org/logstash/plugins/inputs/http/util/SslSimpleBuilder.java
+++ src/main/java/org/logstash/plugins/inputs/http/util/SslSimpleBuilder.java
@@ -37,8 +37,8 @@ public class SslSimpleBuilder implements SslBuilder {
     static {
         String[] defaultCipherCandidates = new String[] {
             // Modern compatibility
-            "TLS_AES_128_GCM_SHA256", // TLS 1.3
             "TLS_AES_256_GCM_SHA384", // TLS 1.3
+            "TLS_AES_128_GCM_SHA256", // TLS 1.3
             "TLS_CHACHA20_POLY1305_SHA256", // TLS 1.3 (since Java 11.0.14)
             // Intermediate compatibility
             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
