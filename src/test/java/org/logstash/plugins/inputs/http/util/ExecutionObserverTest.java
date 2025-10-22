a3b2d839a389e5d585bbcffbe24c72f637dca17c
Cas Donoghue <cas.donoghue@gmail.com>
2025-10-22 09:42:21 -0700

    Update src/test/java/org/logstash/plugins/inputs/http/util/ExecutionObserverTest.java
    Co-authored-by: Rye Biesemeyer <yaauie@users.noreply.github.com>

--- src/test/java/org/logstash/plugins/inputs/http/util/ExecutionObserverTest.java
+++ src/test/java/org/logstash/plugins/inputs/http/util/ExecutionObserverTest.java
@@ -191,7 +191,7 @@ class ExecutionObserverTest {
             assertThat(maxConcurrency.get(), is(greaterThan(1)));
             assertThat(maxNodes.get(), is(lessThanOrEqualTo(concurrency * 2)));
 
-            // without queries, we should at least have some compaction
+            // without queries, we may have some compaction so we shouldn't be too strict
             final ExecutionObserver.Stats preCompactionStats = observer.stats();
             assertThat(preCompactionStats.executing, is(0));
             assertThat(preCompactionStats.nodes, is(both(greaterThan(0)).and(lessThan((int) Math.sqrt(concurrency)))));
